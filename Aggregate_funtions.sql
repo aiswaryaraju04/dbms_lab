3c1f23aa3e472315d7a6ab2d580d2d10b82c7150
Aiswarya Raju <aiswaryaraju2001@gmail.com>
2021-12-22 14:06:44 +0530

    New file

---
+++ Aggregate_funtions.sql
@@ -0,0 +1,24 @@
+--Create a table named student and populate the table as shown in the table.
+--The table contains the marks of 10 students for 3 subjects(Physics, Chemistry, Mathematics).The total marks for physics and chemistry is 25.while for
+   mathematics it is 50.The pass mark for physics and chemistry is 12 and for mathematics it is 25. A student is awarded a ‘Pass’ if he has passed all the subjects.
+
+
+--1. Find the class average for the subject ‘Physics’
+
+--2. Find the highest marks for mathematics (To be displayed as highest_marks_maths).
+
+--3. Find the lowest marks for chemistry(To be displayed as lowest_mark_chemistry)
+
+--4. Find the total number of students who has got a ‘pass’ in physics.
+
+--5. Generate the list of students who have passed in all the subjects
+
+--6. Generate a ranklist for the class.Indicate Pass/Fail. Ranking based on total marks obtained by the students.
+
+--7. Find pass percentage of the class for mathematics
+
+--8. Find the overall pass percentage for all class.
+
+--9. Find the class average.
+
+--10. Find the total number of students who have got a Pass.--
